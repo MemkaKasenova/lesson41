db0185d7d9b4a42aac2b1e3ee608c81c2d26a6bb
Memka <mkasenova87@gmail.com>
2023-04-08 19:05:48 +0600

    Initial Commit

---
+++ lesson41/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson41
+//
+//  Created by merim kasenova on 8/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
